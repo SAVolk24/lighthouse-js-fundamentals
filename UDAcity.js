9a9ec0a9944350d87942431757121d9a54e05250
Stephen Volk <savolk24@gmail.com>
2022-07-12 15:43:14 -0400

    Updated the UDAcity file with more examples

--- UDAcity.js
+++ UDAcity.js
@@ -108,31 +108,123 @@
 // console.log(category)
 
 
-let education = "a Doctoral degree";
-let salary = 0
+// let education = "a Doctoral degree";
+// let salary = 0
 
-switch (education) {
-  case "no high school diploma":
-    salary = 25636;
-    break;
-  case "a high school diploma":
-    salary = 35256;
-    break;
-  case "an Associates degree":
-    salary = 41496;
-    break;
-  case "a Bachelor's degree":
-    salary = 59124;
-    break;
-  case "a Master's degree":
-    salary = 69732;
-    break;
-  case "a Professional degree":
-    salary = 89960;
-    break;
-  case "a Doctoral degree":
-    salary = 84396;
-    break;
-}
+// switch (education) {
+//   case "no high school diploma":
+//     salary = 25636;
+//     break;
+//   case "a high school diploma":
+//     salary = 35256;
+//     break;
+//   case "an Associates degree":
+//     salary = 41496;
+//     break;
+//   case "a Bachelor's degree":
+//     salary = 59124;
+//     break;
+//   case "a Master's degree":
+//     salary = 69732;
+//     break;
+//   case "a Professional degree":
+//     salary = 89960;
+//     break;
+//   case "a Doctoral degree":
+//     salary = 84396;
+//     break;
+// }
 
-console.log("In 2015, a person with " + education + " earned an average of $" + salary.toLocaleString("en-US") + "/year");
+// console.log("In 2015, a person with " + education + " earned an average of $" + salary.toLocaleString("en-US") + "/year");
+
+/////////////////////// LOOOOOOOOOOOOOPS ////////////////////
+
+
+// var start = 0; // when to start
+// while (start < 10) { // when to stop
+//   console.log(start);
+//   start = start + 2; // how to get to the next item
+// }
+
+// var x = 21;
+
+// for (let i = 1; i < 21; i++) {
+//   if (i % 3 === 0 && i % 5 === 0){
+//     console.log("JuliaJames");
+//   } else if (i % 3 === 0) {
+//     console.log("Julia");
+//   } else if (i % 5 === 0) {
+//     console.log("James");
+//   } else {
+//     console.log(i);
+//   }
+// };
+
+///////////// While Loop variation of FizzBuzz ///////////////
+// var x = 1;
+// while (x < 21) {
+//   if (x % 3 === 0 && x % 5 ===0) {
+//     console.log("JuliaJames");
+//   } else if (x % 3 === 0) {
+//     console.log("Julia");
+//   } else if (x % 5 === 0) {
+//     console.log("James");
+//   } else {
+//     console.log(x)
+//   }
+//   x++
+// }
+
+
+/////////////// While loop for 99 bottles of beer ///////////////
+// var num = 99;
+
+// while (num >= 1) {
+//   if (num === 1) {
+//     console.log(num + " bottle of juice on the wall! " + num + " bottle of juice! Take one down, pass it around... " + (num - 1) + " bottles of juice on the wall!");
+//   } else if (num === 2) {
+//     console.log(num + " bottles of juice on the wall! " + num + " bottles of juice! Take one down, pass it around... " + (num - 1) + " bottle of juice on the wall!");
+//   } else {
+//     console.log(num + " bottles of juice on the wall! " + num + " bottles of juice! Take one down pass it around... " + (num - 1) + " bottles of juice on the wall!");
+//   }
+//   num = num - 1;
+// }
+
+    // var seconds = 60;
+
+    // while (seconds >= 0) {
+    //   if (seconds === 50) {
+    //     console.log("Orbiter transfers from ground to internal power");
+    //   } else if (seconds === 31) {
+    //     console.log("Ground launch sequencer is go for auto sequence start");
+    //   } else if ( seconds === 16) {
+    //     console.log("Activate launch pad sound suppression system");
+    //   } else if (seconds === 10) {
+    //     console.log("Activate main engine hydrogen burnoff system");
+    //   } else if (seconds === 6) {
+    //     console.log("Main engine start");
+    //   } else if (seconds === 0) {
+    //     console.log("Solid rocket booster ignition and liftoff!")
+    //   } else {
+    //     console.log("T-"+seconds+" seconds");
+    //   }
+    //   seconds = seconds - 1;
+    // }
+
+    ////////////////// FOR LOOP EXAMPLE //////////////////
+
+    // for (var x = 9; x >= 1; x--) {
+    //   console.log("hello " + x);
+    // }
+
+    //let solution = 1; // delcare solution and set it to = 1
+
+    //for(let i = 1; i <= 12; i++) {       // For Loop takes the solution variable and as long as i which is set to 1 is less or equal to 5 it keeps adding one
+    //  solution *= i;                    // now it takes solution and multiples it by i
+    //}
+
+    //console.log(solution);              // log the solution
+
+    for (let i = 0; i <= 6; i += 2) {
+      console.log(i);
+    }
