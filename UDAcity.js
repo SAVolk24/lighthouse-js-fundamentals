cbe433b0e1bf2ff841b3283a755f05ed8fd7b6e8
Stephen Volk <savolk24@gmail.com>
2022-08-18 15:18:36 -0400

    Made changes to the solution

--- UDAcity.js
+++ UDAcity.js
@@ -371,3 +371,66 @@
 // };
 
 // console.log(numbers);
+
+
+
+
+
+//////////////////// OBJECTS /////////////////////
+
+// var savingsAccount = {
+//     balance: 1000,
+//     interestRatePercent: 1,
+//     deposit: function addMoney(amount) {
+//         if (amount > 0) {
+//             savingsAccount.balance += amount;
+//         }
+//     },
+//     withdraw: function removeMoney(amount) {
+//         var verifyBalance = savingsAccount.balance - amount;
+//         if (amount > 0 && verifyBalance >= 0) {
+//             savingsAccount.balance -= amount;
+//         }
+//     },
+//     // your code goes here
+//     printAccountSummary: function () {
+//         return "Welcome!\nYour balance is currently $" + savingsAccount.balance + " and your interest rate is " + savingsAccount.interestRatePercent + "%." 
+//     }
+// };
+
+// console.log(savingsAccount.printAccountSummary());
+// console.log(savingsAccount.removeMoney(50));
+
+
+/////////////// UPDATING FACEBOOK PROFILE OBJECT /////////////////
+
+// let facebookProfile = {
+//     name: "Stephen",
+//     friends: 151,
+//     messages: ["Message 1", "Message 2", "Message 3"],
+//     postMessage: function (message) {
+//         facebookProfile.messages.push(message);
+//     },
+//     deleteMessage: function (index) {
+//         facebookProfile.messages.splice(index, 1);
+//     },
+//     addFriend: function () {
+//         facebookProfile.friends = facebookProfile.friends + 1
+//     },
+//     removeFriend: function() {
+//         facebookProfile.friends = facebookProfile.friends - 1
+//     }
+// };
+
+var donuts = [
+    { type: "Jelly", cost: 1.22 },
+    { type: "Chocolate", cost: 2.45 },
+    { type: "Cider", cost: 1.59 },
+    { type: "Boston Cream", cost: 5.99 }
+];
+
+// your code goes here
+donuts.forEach(function(donut) {
+    console.log(donut.type + " donuts cost $" + donut.cost + " each") 
+});
+
--- tinkering.js
+++ tinkering.js
@@ -1,6 +1,5 @@
 //////////// LHL Which School ///////////////
 
-
 // const whichSchool = function (age) {
 //   if (age < 13) {
 //     return "Elementary School";
@@ -39,7 +38,6 @@
 
 // console.log('Order total is: ' + total);
 
-
 ///////////// REVISIT THIS WHEN FINISHED WITH PREP WORK TO TRY AGAIN WITHOUT AN AID ///////////////////////
 // const stations = [
 //   ['Big Bear Donair', 10, 'restaurant'],
@@ -61,7 +59,6 @@
 // }
 
 // console.log(chooseStations(stations));
-
 
 /////////////// COME BACK AND REVIEW THIS CODE SNIPPET FOR MOVING ON THE MAP //////////////////////
 
@@ -93,7 +90,6 @@
 
 // console.log(age("stephen", 1984, 2022));
 
-
 /////////////// AREA CALCULATOR /////////////////////
 // const rectangle = function (length, width) {
 //   if (length < 0 || width < 0) {
@@ -119,10 +115,149 @@
 //   }
 // };
 
-
 /////////// CODE SNIPPET TO SEE HOW MANY HUNDREDS ///////////////////
 // const howManyHundreds = function(hundreds) {
 //   return Math.floor(hundreds / 100);
 //  }
- 
+
 //  console.log(howManyHundreds(894));
+
+////////////////////// LOOPING RANGE USING FOR LOOP ////////////////////
+
+// const range = function (start, end, step) {
+//   let result = [];
+//   if (step > 0) {
+//     for (i = start; i <= end; i+= step) {
+//       result.push(i);
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(range(0, 10, 2));
+// console.log(range(10, 30, 5));
+// console.log(range(-5, 2, 0));
+
+//////////// GEWTTING THE LAST NUMBER IN THE ARRAY TO PRINT ////////////////////
+
+// function lastIndexOf(array, num) {
+//   for (let i = array.length - 1; i >= 0; i--) {
+//     if (array[i] === num) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
+
+// console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+// console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+// console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+// console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+// console.log(lastIndexOf([], 3), "=?", -1);
+
+///////////////// CONCAT 2 ARRAYS TOGETHER //////////////////
+
+// const concat = function (array1, array2) {
+//   let array3 = [];
+//   array3 = array1.concat(array2);
+//   return array3;
+// }
+
+// console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+
+// var myObj = {
+//   color: "orange",
+//   shape: "sphere",
+//   type: "food",
+//   eat: function() { return "yummy" }
+// };
+
+// myObj.color;
+
+//////////// Trash to Treasure Quiz /////////////
+
+// const smartGarbage = function (trash, bins) {
+//   bins[trash] += 1;
+//   return bins;
+// }
+
+// console.log(smartGarbage('compost', {waste: 10, recycling: 25, compost: 19}));
+
+////////////////// DRIVING MAYOR DAISY //////////////////////
+
+// const carPassing = function (cars, speed) {
+//   // Your code in here ...
+//   const carsArray = {
+//     time: Date.now(),
+//     speed: speed
+//   }
+//   cars.push(carsArray);
+//   return cars;
+
+// }
+
+////////////// WE'RE ROOTING FOR YOU /////////////////
+
+// const judgeVegetable = function (vegetables, metric) {
+//   let value = 0;
+//   let submitter = '';
+
+//   for (let i = 0; i < vegetables.length; i++) {
+//     if (value < vegetables[i][metric]) {
+//       value = vegetables[i][metric];
+//       submitter = vegetables[i].submitter;
+//     }
+//   }
+//   return submitter;
+// }
+
+var shirtWidth = 18.75;
+var shirtLength = 28.56;
+var shirtSleeve = 8.25;
+
+if (
+  shirtWidth >= 18 &&
+  shirtWidth <= 19.99 &&
+  shirtLength >= 28 &&
+  shirtLength <= 28.99 &&
+  shirtSleeve >= 8.13 &&
+  shirtSleeve <= 8.37
+) {
+  console.log("S");
+} else if (
+  shirtWidth >= 18 &&
+  shirtWidth <= 19.99 &&
+  shirtLength >= 28 &&
+  shirtLength <= 28.99 &&
+  shirtSleeve >= 8.13 &&
+  shirtSleeve <= 8.37
+) {
+  console.log("M");
+} else if (
+  shirtWidth >= 18 &&
+  shirtWidth <= 19.99 &&
+  shirtLength >= 28 &&
+  shirtLength <= 28.99 &&
+  shirtSleeve >= 8.13 &&
+  shirtSleeve <= 8.37
+) {
+  console.log("L");
+} else if (
+  shirtWidth >= 18 &&
+  shirtWidth <= 19.99 &&
+  shirtLength >= 28 &&
+  shirtLength <= 28.99 &&
+  shirtSleeve >= 8.13 &&
+  shirtSleeve <= 8.37
+) {
+  console.log("XL");
+} else if (
+  shirtWidth >= 18 &&
+  shirtWidth <= 19.99 &&
+  shirtLength >= 28 &&
+  shirtLength <= 28.99 &&
+  shirtSleeve >= 8.13 &&
+  shirtSleeve <= 8.37
+) {
+  console.log("2XL");
+}
